addd3cb6bb873179f7063e5f9b35c20892611c1a
DavidGandi <david.gandi@metis.academy>
2024-06-19 21:51:47 +0200

    21.3.1

---
+++ java-basics/src/main/java/academy/metis/javabasics/lesson21/activity3/exercise1/part1/LibraryApp.java
@@ -0,0 +1,37 @@
+package academy.metis.javabasics.lesson21.activity3.exercise1.part1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LibraryApp {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final InputValidator validator = new InputValidator(scanner);
+
+    public static void main(String[] args)  {
+        System.out.println(">>>> Welcome to our Library <<<<");
+        showMainMenu();
+    }
+    public static void showMainMenu() {
+
+        int numberOfCase = 4;
+
+        System.out.println("Main menu:\n1 - Titles\n2 - Members\n3 - Rentals\n4 - Exit");
+        System.out.println("Choose an option: ");
+
+        switch (validator.getValidatedChoice(numberOfCase)) {
+            case 1:
+                TitlesPage titlesPage = new TitlesPage(scanner);
+                titlesPage.showTitlesMenu();
+                break;
+            case 2:
+                MembersPage membersPage = new MembersPage(scanner);
+                membersPage.showMembersMenu();
+                break;
+            case 3:
+                showMainMenu();
+                break;
+            case 4:
+                break;
+        }
+    }
+}
